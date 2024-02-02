1aa1b9f1740d3c6651f8ac4d6fc0887bb8e83e4d
Daniel Oropeza <74103555+ferdoropeza@users.noreply.github.com>
2024-02-01 21:57:20 -0500

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 const path = require("path");
-const app = express();
-const port = 3000;
 
 // Links used for the redirect
 const OBJKT_LINK =
@@ -13,83 +13,98 @@ const SUPERRARE_LINK =
 const TWITTER_LINK =
   "https://twitter.com/ferdoropeza";
 
-// Middleware to parse JSON body
-app.use(express.json());
+const server = http.createServer((req, res) => {
+  // GET / (Index Route)
+  // Return a frame which renders an image with four redirect buttons
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`
+      <html>
+         <head>   
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width"/>
+            <meta property="og:title" content="FC Dev Call" />
+            <meta property='og:image' content="https://github.com/ferdoropeza/Farcaster-Dev-Call-Frame/blob/main/frame-fc.png?raw=true" />
+            <meta property="fc:frame" content="vNext" />
+            <meta property="fc:frame:image" content="https://github.com/ferdoropeza/Farcaster-Dev-Call-Frame/blob/main/frame-fc.png?raw=true" />
+            <meta property="fc:frame:button:1" content="SuperRare" />
+            <meta property="fc:frame:button:1:action" content="post_redirect" />
+            <meta property="fc:frame:button:2" content="Objkt" />
+            <meta property="fc:frame:button:2:action" content="post_redirect" />
+            <meta property="fc:frame:button:3" content="Twitter" />
+            <meta property="fc:frame:button:3:action" content="post_redirect" />
+            <meta property="fc:frame:button:4" content="Fxhash" />
+            <meta property="fc:frame:button:4:action" content="post_redirect" />
+            <meta property="fc:frame:post_url" content="https://farcaster-dev-call-frame.vercel.app/click" />
+          </head>
+          <body><marquee> wowow </marquee></body>
+      </html>`);
+    res.end();
 
-// GET / (Index Route)
-app.get("/", (req, res) => {
-  res.send(`
-    <html>
-       <head>   
-          <meta charSet="utf-8"/>
-          <meta name="viewport" content="width=device-width"/>
-          <meta property="og:title" content="FC Dev Call" />
-          <meta property='og:image' content="https://raw.githubusercontent.com/ferdoropeza/Farcaster-Dev-Call-Frame/main/frame-fc.png" />
-          <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="https://raw.githubusercontent.com/ferdoropeza/Farcaster-Dev-Call-Frame/main/frame-fc.png" />
-          <meta property="fc:frame:button:1" content="SuperRare" />
-          <meta property="fc:frame:button:1:action" content="post_redirect" />
-          <meta property="fc:frame:button:2" content="Objkt" />
-          <meta property="fc:frame:button:2:action" content="post_redirect" />
-          <meta property="fc:frame:button:3" content="Twitter" />
-          <meta property="fc:frame:button:3:action" content="post_redirect" />
-          <meta property="fc:frame:button:4" content="FxHash" />
-          <meta property="fc:frame:button:4:action" content="post_redirect" />
-          <meta property="fc:frame:post_url" content="https://farcaster-dev-call-frame.vercel.app/click" />
-        </head>
-        <body><marquee behavior="scroll" direction="left">wowow!</marquee></body>
-    </html>
-    `);
-});
+    // GET /image
+    // Return the image used in the image tag
+  } else if (req.url === "/image") {
+    const imagePath = path.join(__dirname, "frame-fc.png");
+    const imageStream = fs.createReadStream(imagePath);
+    res.writeHead(200, { "Content-Type": "image/png" });
+    imageStream.pipe(res);
 
-// GET /image
-// Return the image used in the image tag
-app.get("/image", (req, res) => {
-  const imagePath = path.join(__dirname, "frame-fc.png");
-  res.sendFile(imagePath);
-});
+    // POST /click
+    // Handle the Frame Signature Packet response when a button is clicked
+    // in the frame
+  } else if (req.method === "POST" && req.url === "/click") {
+    let body = "";
 
-// POST /click
-// Callback which handles the button click event. Unpacks the Frame Signature
-// packet to extract the button clicked and forwards it to /redirect. Using
-// untrusted data is safe because we take the same action irrespective if who
-// clicked the frame button.
-app.post("/click", (req, res) => {
-  const buttonIndex = req.body.untrustedData.buttonIndex;
-  console.log("0");
-  res.redirect(`/redirect?buttonIndex=${buttonIndex}`);
-});
+    req.on("data", (chunk) => {
+      body += chunk.toString(); // convert Buffer to string
+    });
 
-// GET /redirect
-// Redirects to an external URL based on buttonIndex parameter. Used to work
-// around the same origin policy  on frames, which is being removed soon.
-app.get("/redirect", (req, res) => {
-  console.log("1");
-  const buttonIndex = req.query.buttonIndex;
-  let redirectUrl;
+    req.on("end", () => {
+      // Get the index of the button that was clicked. Using untrusted data is
+      // safe because we take the same action irrespective of who clicked it.
+      const buttonIndex = JSON.parse(body).untrustedData.buttonIndex;
 
-  switch (buttonIndex) {
-    case "1":
-      console.log("2b");
+      // Route to another internal endpoint which does the redirect.
+      res.writeHead(302, {
+        Location:
+          "https://farcaster-dev-call-frame.vercel.app/redirect?buttonIndex=" + buttonIndex,
+      });
+
+      res.end();
+    });
+
+    // GET /redirect
+    // An endpoint which accepts a button index and 302's to the appropriate URL
+    // This works around the same-origin policy that frames enforce for
+    // post_redirects which is being fixed soon.
+  } else if (req.method === "GET" && req.url.startsWith("/redirect")) {
+    const queryObject = url.parse(req.url, true).query;
+    const buttonIndex = queryObject.buttonIndex;
+
+    let redirectUrl;
+
+    if (buttonIndex === "1") {
       redirectUrl = SUPERRARE_LINK;
-      break;
-    case "2":
+    } else if (buttonIndex === "2") {
       redirectUrl = OBJKT_LINK;
-      break;
-    case "3":
+    } else if (buttonIndex === "3") {
       redirectUrl = TWITTER_LINK;
-      break;
-    case "4":
+    } else if (buttonIndex === "4") {
       redirectUrl = FXHASH_LINK;
-      break;
-    default:
-      console.log("2a");
-      return res.status(404).send("Invalid button index");
-  }
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Invalid button index");
+    }
 
-  res.redirect(redirectUrl);
+    res.writeHead(302, { Location: redirectUrl });
+    res.end();
+  } else {
+    // Catchall 404 Route
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Page not found");
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
